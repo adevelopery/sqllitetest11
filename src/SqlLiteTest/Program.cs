684338f3a9d1c94bbbfe6353eff3bc1b4c2e804f
Aleš Rudolf <Ales.Rudolf@stroka.si>
2017-05-03 13:53:55 +0200

    made a change

--- src/SqlLiteTest/Program.cs
+++ src/SqlLiteTest/Program.cs
@@ -13,7 +13,7 @@ namespace SqlLiteTest
             {
                 db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
                 var count = db.SaveChanges();
-                Console.WriteLine("Shranjenih zapisov: {0}", count);
+                Console.WriteLine("SSSShranjenih zapisov: {0}", count);
 
                 Console.WriteLine();
                 Console.WriteLine("All blogs in database:");
